4536f21fb45036cccb261b3f2549304590c06b98
Muhammad Qazi <tigerbhai888@gmail.com>
2022-07-07 10:30:05 +0300

    Day-016

---
+++ day-016/WeSplit.swift
@@ -0,0 +1,16 @@
+struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+    }
+}
